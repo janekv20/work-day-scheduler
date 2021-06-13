7297da923459cd67de9d3b09c49724d18a395e80
Pint <83248746+janekv20@users.noreply.github.com>
2021-06-13 09:59:20 -0500

    fixed script.js by adding hour to id and reformatting script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,54 @@
-// Show today's day and date
-var todayDate = moment().format('dddd, MMM Do YYYY');
-$("#currentDay").html(todayDate);
-
 $(document).ready(function () {
-    // saveBtn click  
+    //Show today's date and time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+    //saveBtn click listener for user input
     $(".saveBtn").on("click", function () {
-        // Get nearby values of the description in JQuery
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
+        //get nearby values.
+        console.log(this);
+        var text = $(this).siblings(".description").val(); // taken the change from the sibling html description attribute
+        var time = $(this).parent().attr("id"); // taken the change from the parent html id attribute
 
-        // Save text input in local storage
+        //Save items in local storage
         localStorage.setItem(time, text);
     })
+    //Get item from local storage
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-    function timeTracker() {
-        //get current number of hours.
-        var timeNow = moment().hour();
+    function hourTracker() {
+        //get current number of hours using moment.js
+        var currentHour = moment().hour();
 
-        // time block loop
+        // loop over time blocks
         $(".time-block").each(function () {
-            var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+            console.log(blockHour, currentHour)
 
-            // Check the time and add classes for background indicators
-            if (blockTime < timeNow) {
+            //check the time, click into css/html given classes of past, present, or future
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
                 $(this).removeClass("future");
                 $(this).removeClass("present");
-                $(this).addClass("past");
             }
-            else if (blockTime === timeNow) {
+            else if (blockHour === currentHour) {
                 $(this).removeClass("past");
-                $(this).removeClass("future");
                 $(this).addClass("present");
+                $(this).removeClass("future");
             }
             else {
                 $(this).removeClass("present");
                 $(this).removeClass("past");
                 $(this).addClass("future");
-
             }
         })
     }
-
-    // Get item from local storage if any
-    $("#8 .description").val(localStorage.getItem("8"));
-    $("#9 .description").val(localStorage.getItem("9"));
-    $("#10 .description").val(localStorage.getItem("10"));
-    $("#11 .description").val(localStorage.getItem("11"));
-    $("#12 .description").val(localStorage.getItem("12"));
-    $("#13 .description").val(localStorage.getItem("13"));
-    $("#14 .description").val(localStorage.getItem("14"));
-    $("#15 .description").val(localStorage.getItem("15"));
-    $("#16 .description").val(localStorage.getItem("16"));
-    $("#17 .description").val(localStorage.getItem("17"));
-
-    timeTracker();
+    hourTracker(); // run the function
 })
